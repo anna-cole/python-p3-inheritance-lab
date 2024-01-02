ed388724fb209aadd827dee05bce7eec1c1e23dd
Anna Raptidou Cole <acrrj123@gmail.com>
2024-01-02 17:37:54 -0600

    Lab done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,14 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
-        pass
+    def learn(self, phrase):
+        self.knowledge.append(phrase)
+         
+# student = Student("ana", "cole")
+# test = student.learn("eu sou a ana")
+# print(test) put a return before the last line of code
+        
